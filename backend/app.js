53ac76209155234f2e73ddd11480200aaa2a5564
Areeb610 <areebbutt610@gmail.com>
2024-01-17 15:34:44 +0500

    Added vercel.json

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cors=require("cors");
 const corsOptions ={
-   origin:'https://react-app-server-eiuz5vmkl-areeb610.vercel.app/tasks', 
+   origin:'https://react-app-client-one.vercel.app', 
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }
